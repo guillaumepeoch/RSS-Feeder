331c5855b83269c5a23f9e32c688f7c071916455
guillaumepeoch <guillaume.peoch@gmail.com>
2019-01-26 18:45:10 -0800

    side menu sources

--- src/components/side_menu/SideMenu.js
+++ src/components/side_menu/SideMenu.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './sidemenu.module.css';
 
-const SideMenu = function(){
-  return(
-    <div className={styles.menu}>
-      <p>News</p>
-    </div>
-  );
+class SideMenu  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sources:['Washington Post', 'NY Times', 'NPR']
+    };
+  }
+  
+  render(){
+    return(
+      <ul className={styles.menu}>
+        { this.state.sources.map(function(link, index){ 
+          return (
+            <li className={styles.source}>{link}</li>
+          );
+         }) }
+      </ul>
+    );
+  }
 }
 
 export default SideMenu;
