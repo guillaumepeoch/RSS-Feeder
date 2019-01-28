a40aa92167798febe3b11c4478c498812c96484f
guillaumepeoch <guillaume.peoch@gmail.com>
2019-01-27 16:23:32 -0800

    backend set up -> node + express + body parser

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// EXPRESS
+const express = require('express');
+const app = express();
+
+app.get('/',function(req,res){
+  res.send('Yoyo!');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port);
